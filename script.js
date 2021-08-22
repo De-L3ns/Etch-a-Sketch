05f619113e17ad6f825b6dc72f6296648e4a427b
De-L3ns <laurensviaene@gmail.com>
2021-08-22 17:01:55 +0200

    made interaction with grid possible and changing grid to css flexbox

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 // functions
 
-const gridContainer = document.querySelector('#grid');
-amount = 256;
-
-
 function createGrid(squares) { // creating the divs
     
     for (i = 0; i < squares; i += 1) {
     const squareDiv = document.createElement('div');
+    squareDiv.classList.add('square');
     gridContainer.appendChild(squareDiv);
+    
 
     }
 }
 
+
+// variables and gameloop 
+const gridContainer = document.querySelector('#grid');
+let amount = 256;
 createGrid(amount);
+
+const getSquares = document.querySelectorAll('.square'); // this checks all div's with a .square class and 
+getSquares.forEach(square => {                            // changes the background-color to blue when hovered
+    square.addEventListener('mouseover', function(e) {
+        e.target.style.background = 'blue';
+    })
+})
+
+
