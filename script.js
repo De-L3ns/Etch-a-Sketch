3b0da32ca05dfec9a280a22a07e5000375b1a719
De-L3ns <laurensviaene@gmail.com>
2021-08-27 13:05:30 +0200

    updated layout and added clear grid button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // functions
 
-function createGrid(squares) { // creating the divs
+function createGrid(squares) { // Creating the divs for the grid
     
     for (i = 0; i < squares; i += 1) {
     const squareDiv = document.createElement('div');
@@ -10,29 +10,48 @@ function createGrid(squares) { // creating the divs
 
     }
 }
-
-function refreshGrid(squares) { // this sets all current squares in the grid back to 0.
-    squares.forEach(square => {
-        square.setAttribute('style', 'background: grey');
+function manipulateGrid() {
+    const getSquares = document.querySelectorAll('.square'); // This checks all div's with a .square class and 
+    getSquares.forEach(square => {                            // changes the background-color to blue when hovered
+        square.addEventListener('mouseover', function(e) {
+            e.target.style.background = 'blue';
+        })
     })
 }
+
+function removeChilds(parent) { // This loop removes the parent divs until there are
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 
 // variables and logic 
 const gridContainer = document.querySelector('#grid');
 let squareAmount = 256;
-createGrid(squareAmount);
+createGrid(squareAmount); // Initialises the basic 16x16 grid 
+manipulateGrid(); // Initialises the ability to change grid color when hovering
 
-const getSquares = document.querySelectorAll('.square'); // this checks all div's with a .square class and 
-getSquares.forEach(square => {                            // changes the background-color to blue when hovered
-    square.addEventListener('mouseover', function(e) {
-        e.target.style.background = 'blue';
-    })
-})
-
-
-const resetGrid = document.getElementById('reset');
-console.log(resetGrid);
+// button logic
+const resetGrid = document.getElementById('reset'); // Clears the grid and sets it back to it's original state
 resetGrid.addEventListener('click', () => {
-    refreshGrid(getSquares);
+    removeChilds(gridContainer);
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(16, 2fr); grid-template-rows: repeat(16, 2fr);`);
+    createGrid(squareAmount);
+    manipulateGrid();
+});
+
+const getInput = document.getElementById('input'); // Gets user input from the text field and applies it to the grid
+getInput.addEventListener('click', () => {
+    let value = document.getElementById('grid-value').value;
+    if (value > 100) {
+        alert('Please enter a number below 100');
+    }
+    else {
+        removeChilds(gridContainer);
+        gridContainer.setAttribute('style', `grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);`);
+        createGrid(value*value);
+        manipulateGrid();
+    }
 });
 
